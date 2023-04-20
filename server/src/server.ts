744e0ce0ae22cd0df4b88db0c8a314e73095d11d
carraes <carraeshb@gmail.com>
2023-04-20 18:35:43 -0300

    chore: create server/ folder and move files/adapt context

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import morgan from 'morgan'
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = 8080
 
 app.use(express.json())
 app.use(morgan('dev'))
