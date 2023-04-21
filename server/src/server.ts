1db472726765991dc3decda7ef30d32b63a8a58e
Carlos <carraeshb@gmail.com>
2023-04-20 23:27:00 -0300

    merge: add pkgs, some tsconfig changes, restructure folders (#1)
    * chore: add env to ignore, add drizzle/planetscale pkgs
    
    * chore: create server/ folder and move files/adapt context
    
    * chore: tsconfig changes

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,15 +2,15 @@ import express from 'express'
 import morgan from 'morgan'
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = 8080
 
 app.use(express.json())
 app.use(morgan('dev'))
 
 app.get('/', (_, res) => {
-  res.status(200).json({ message: 'Hello CI/CD :)!' })
+  res.status(200).json({ message: 'Hello' })
 })
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`)
+  console.log(`Server running at PORT:${port}`)
 })
